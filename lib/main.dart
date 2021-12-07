6c8d001df7ddc4d0fdb42d3d18d04cbf0830f97c
amitgain1818 <amitgain1818@gmail.com>
2021-12-07 16:09:02 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.cyan,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.cyan,
+        ),
+        body: LuduGame(),
+      ),
+    ),
+  );
+}
+
+class LuduGame extends StatefulWidget {
+  @override
+  _LuduGameState createState() => _LuduGameState();
+}
+
+class _LuduGameState extends State<LuduGame> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
